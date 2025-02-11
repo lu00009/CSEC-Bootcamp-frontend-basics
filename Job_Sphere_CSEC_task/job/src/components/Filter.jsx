5b8b71a00db2ed68a2b6fd5236337c15c4971388
lu00009 <lelomohammedpg@gmail.com>
2025-02-11 22:42:17 +0300

    added range slider

--- Job_Sphere_CSEC_task/job/src/components/Filter.jsx
+++ Job_Sphere_CSEC_task/job/src/components/Filter.jsx
@@ -1,74 +1,172 @@
-import React from 'react'
+import {useState} from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { IoLocationOutline } from "react-icons/io5";
 
-
 const Filter = () => {
+  const [minValue, setMinValue] = useState(20);
+  const [maxValue, setMaxValue] = useState(2000);
+  const minLimit = 20;
+  const maxLimit = 2000;
+
+  const handleMinChange = (e) => {
+    const value = Math.min(Number(e.target.value), maxValue - 1);
+    setMinValue(value);
+  };
+
+  const handleMaxChange = (e) => {
+    const value = Math.max(Number(e.target.value), minValue + 1);
+    setMaxValue(value);
+  };
+
+  const minPercent = ((minValue - minLimit) / (maxLimit - minLimit)) * 100;
+  const maxPercent = ((maxValue - minLimit) / (maxLimit - minLimit)) * 100;
+
   return (
-    <div className='pl-3.5 w-[30lvw] rounded-[8px] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) h-[500px] border-[1px] border-[#000000]'>
-      <h2 className='ml-10 font-bold text-[22px]'>Filter</h2>
-      <div><h3>Date Posted</h3>
-      <div className='flex justify-between pt-[7px] pr-[12px] pb-[7px] pl-[12px] bg-white border-[1px] rounded-[10px] h-[33px] w-[213px]'><button className='mb-3'>Last 24 Hours</button>
-      <RiArrowDropDownLine />
-
+    <div className="pl-3.5 w-[30lvw] rounded-[8px] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) h-[570px] border-[1px] border-[#000000]">
+      <h2 className="ml-10 font-bold text-[22px]">Filter</h2>
+      <div>
+        <h3 className="mt-2">Date Posted</h3>
+        <div className="flex justify-between pt-[7px] pr-[12px] pb-[7px] pl-[12px] bg-white border-[1px] rounded-[10px] h-[33px] w-[213px]">
+          <button className="mb-3">Last 24 Hours</button>
+          <RiArrowDropDownLine />
+        </div>
       </div>
+      <div className="mt-2.5">
+        <h3 >Job Type</h3>
+        <div className="rounded-[8px] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25)  w-[213px] border-[1px] border-[#000000] pl-2 pt-2">
+          <ul>
+            {" "}
+            <input type="checkbox" value="" name="check-1" />
+            <label htmlFor="check-1">Full-time</label>
+          </ul>
+          <ul>
+            <input type="checkbox" value="" name="check-2" />
+            <label htmlFor="check-2">Part-time</label>
+          </ul>
+          <ul>
+            <input type="checkbox" value="" name="check-3" />
+            <label htmlFor="check-3">Internship</label>
+          </ul>
+          <ul>
+            <input type="checkbox" value="" name="check-4" />
+            <label htmlFor="check-4">Contract</label>
+          </ul>
+          <ul>
+            <input type="checkbox" value="" name="check-5" />
+            <label htmlFor="check-5">Volunteer</label>
+          </ul>
+        </div>
       </div>
-      <div className='mt-1.5'>
-        <h3>Job Type</h3>
-        <div className='rounded-[8px] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25)  w-[213px] border-[1px] border-[#000000] pl-2 pt-2'>
-         <ul> <input type="checkbox" value='' name='check-1' />
-          <label htmlFor="check-1">Full-time</label></ul>
-          <ul><input type="checkbox" value='' name='check-2' />
-          <label htmlFor="check-2">Part-time</label></ul>
-          <ul><input type="checkbox" value='' name='check-3' />
-          <label htmlFor="check-3">Internship</label></ul>
-          <ul><input type="checkbox" value='' name='check-4' />
-          <label htmlFor="check-4">Contract</label></ul>
-          <ul><input type="checkbox" value='' name='check-5' />
-          <label htmlFor="check-5">Volunteer</label></ul>
-        </div>
-        </div> 
-       
-        <div><h3>Location</h3>
-        <div className='flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]'>
-        <IoLocationOutline className='pt-1' />
-        <input type="text" placeholder='Enter your Location' aria-label='Enter location' className='top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px]'
- /></div>
-        
-        </div>
-        
 
-        <div><h3>Experiance Level</h3>
-        <div className='flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]'><button className='top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px] pl-1'>Intermidiet</button>
-      <RiArrowDropDownLine />
+      <div className="mt-2">
+        <h3>Location</h3>
+        <div className="flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]">
+          <IoLocationOutline className="pt-1" />
+          <input
+            type="text"
+            placeholder="Enter your Location"
+            aria-label="Enter location"
+            className="top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px]"
+          />
         </div>
-     
-        </div>
-        <div>
-          <h3>Salary Range</h3>
+      </div>
 
+      <div className="mt-2">
+        <h3>Experiance Level</h3>
+        <div className="flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]">
+          <button className="top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px] pl-1">
+            Intermidiet
+          </button>
+          <RiArrowDropDownLine />
         </div>
-        <div><h3>Input Manually</h3>
-        <div className='flex justify-between w-[113px] '>
-          <div className='flex'><h4>From</h4>
-          <input type="text" className='w-[20px] border-[1px] border-black' /></div>
-          <div className='flex'><h4>To</h4>
-          <input type="text" className='w-[20px] border-[1px] border-black' /></div>
-          
-        </div>
-        </div>
-        <div>
-          <h3>Currency</h3>
-          <div className='flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]'><button className='pl-1'>$Dollar</button>
-      <RiArrowDropDownLine />
-        </div>
-        </div>
-        <div className='w-[140px] h-[30px] rounded-[8px] pt-[4px] pr-[14px] pb-[10px] pl-[30px] gap-[4px] bg-[#0034D1] ml-4 mt-2.5 '>
-          <button className='text-white top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px]'>Reset Filter</button>
-        </div>
-      </div> 
-       
-  )
-}
+      </div>
+      <div className="mt-2">
+        <h3>Salary Range</h3>
+        <div className="pl-3.5 w-[30lvw] rounded-[8px] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25)] h-[570px] border-[1px] border-[#000000]">
+      <h2 className="ml-10 font-bold text-[22px]">Filter</h2>
+      {/* Other filter components... */}
 
-export default Filter
+      <div className="mt-2">
+        <h3>Salary Range</h3>
+        <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md mx-auto p-4">
+          {/* Range Inputs */}
+          <div className="relative w-full">
+            {/* Min Input */}
+            <input
+              type="range"
+              min={minLimit}
+              max={maxLimit}
+              value={minValue}
+              onChange={handleMinChange}
+              className="absolute w-full h-1 appearance-none bg-transparent z-10"
+              style={{ zIndex: minValue > maxValue - 100 ? 5 : 2 }}
+            />
+            {/* Max Input */}
+            <input
+              type="range"
+              min={minLimit}
+              max={maxLimit}
+              value={maxValue}
+              onChange={handleMaxChange}
+              className="absolute w-full h-1 appearance-none bg-transparent z-10"
+              style={{ zIndex: 2 }}
+            />
+
+            {/* Slider Track */}
+            <div className="relative w-full h-1 bg-blue-300 mt-6 rounded">
+              <div
+                className="absolute h-1 bg-blue-700 rounded transition-all duration-300"
+                style={{ left: `${minPercent}%`, right: `${100 - maxPercent}%` }}
+              ></div>
+            </div>
+
+            {/* Draggable Knobs */}
+            <div
+              className="absolute top-3 h-5 w-5 bg-blue-700 rounded-full cursor-pointer transform -translate-x-1/2 z-20"
+              style={{ left: `${minPercent}%` }}
+            ></div>
+            <div
+              className="absolute top-3 h-5 w-5 bg-blue-700 rounded-full cursor-pointer transform -translate-x-1/2 z-20"
+              style={{ left: `${maxPercent}%` }}
+            ></div>
+          </div>
+
+          {/* Min & Max Values */}
+          <div className="flex justify-between mt-3 text-gray-700 text-sm sm:text-base">
+            <span>{minValue}</span>
+            <span>{maxValue.toLocaleString()}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+      </div>
+      <div className="mt-2">
+        <h3>Input Manually</h3>
+        <div className="flex justify-between w-[113px] ml-2 ">
+          <div className="flex">
+            <h4>From</h4>
+            <input type="text" className="w-[20px] border-[1px] border-black" />
+          </div>
+          <div className="flex">
+            <h4>To</h4>
+            <input type="text" className="w-[20px] border-[1px] border-black" />
+          </div>
+        </div>
+      </div>
+      <div className="mt-2">
+        <h3>Currency</h3>
+        <div className="flex justify-between drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25) border-[1px] border-black w-[213px]">
+          <button className="pl-1">$Dollar</button>
+          <RiArrowDropDownLine />
+        </div>
+      </div>
+      <div className="w-[140px] h-[30px] rounded-[8px] pt-[4px] pr-[14px] pb-[10px] pl-[30px] gap-[4px] bg-[#0034D1] ml-4 mt-2.5 ">
+        <button className="text-white top-[6px] left-[37px] font-[300] text-[18px] leading-[21.78px]">
+          Reset Filter
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
--- Job_Sphere_CSEC_task/job/src/components/Searchbar.jsx
+++ Job_Sphere_CSEC_task/job/src/components/Searchbar.jsx
@@ -4,7 +4,7 @@ import { IoLocationOutline } from "react-icons/io5";
 const Searchbar = (prop) => {
   const { query, setQuery, deferredQuery, filteredQuery }= prop
   return (
-    <div className='z-5 fixed w-[500px] h-[38px] top-[90px] left-[300px] rounded-[20px] border-[1px] flex pt-[9px] pr-[12px] pb-[9px] pl-[12px] bg-[#FFFFFF] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25)]'>
+    <div className='z-5 fixed w-[550px] h-[38px] top-[90px] left-[300px] rounded-[20px] border-[1px] flex pt-[9px] pr-[12px] pb-[9px] pl-[12px] bg-[#FFFFFF] drop-shadow-[3px_2px_4px_rgba(0,0,0,0.25)]'>
 
       <div className='w-[308px] h-[24px] flex items-center'>
         <CiSearch className='w-24px h-24px mr-2' />
@@ -48,7 +48,7 @@ const Searchbar = (prop) => {
       </div>
 
       {/* Search Button */}
-      <div className='w-[80px] h-[28px] rounded-[8px] pt-[2px] pr-[14px] pb-[10px] pl-[14px] gap-[4px] bg-[#0034D1] ml-4'>
+      <div className='w-[80px] h-[28px] rounded-[8px] pt-[2px] pr-[14px] pb-[10px] pl-[14px] gap-[4px] bg-[#0034D1] ml-4 mb-2'>
         <button
           className='font-[600] text-[17px] leading-[14px] text-[#FFFFFF]'
           aria-label='Search'
